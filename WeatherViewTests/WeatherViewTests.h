be4e798a737feec19291c696b3c2981a2cbcb03c
tatyusa <kuroneko@wreathlitnoel.com>
2013-06-15 14:45:51 +0900

    Initial Commit

---
+++ WeatherViewTests/WeatherViewTests.h
@@ -0,0 +1,13 @@
+//
+//  WeatherViewTests.h
+//  WeatherViewTests
+//
+//  Created by tatyusa on 6/15/13.
+//  Copyright (c) 2013 tatyusa. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WeatherViewTests : SenTestCase
+
+@end
---
+++ WeatherViewTests/WeatherViewTests.m
@@ -0,0 +1,32 @@
+//
+//  WeatherViewTests.m
+//  WeatherViewTests
+//
+//  Created by tatyusa on 6/15/13.
+//  Copyright (c) 2013 tatyusa. All rights reserved.
+//
+
+#import "WeatherViewTests.h"
+
+@implementation WeatherViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in WeatherViewTests");
+}
+
+@end
